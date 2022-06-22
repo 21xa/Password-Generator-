55b5018eeb51227129f766ba4f0709c8851742d6
21xa <104792225+21xa@users.noreply.github.com>
2022-06-21 21:08:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+var minLength =8
+var maxLength=128
 
 
 // Get references to the #generate element
@@ -12,3 +14,15 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+ var pwLength = window.prompt( "lengh of password")
+ console.log (pwLength)
+ if(pwLength < minLength || pwLength > maxLength){ 
+    window.alert ("password bewtween " + minLength + " and " + maxLength)
+    generatePassword()
+}
+ var lowercase = window.confirm ("yes or no to lowercase")
+ var uppercase = window.confirm ("yes or no to uppercase")
+ var numbers= window.confirm ("yes or no to numbers")
+ var character = window.confirm ("yes or no to special character")
